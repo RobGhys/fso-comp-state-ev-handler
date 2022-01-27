9ba182efe121f84bf85c664268fa3d44c8edddea
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 14:42:02 +0100

    last refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { useState } from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => {
+    return (
+        <button onClick={onClick}>
+        {text}
+    </button>
+    )
+}
+
 const App = () => {
     // Add state to the component and renders it initialized with the value of zero
     // The counter variable is assigned the initial value of state which is zero
     // The variable setCounter is assigned to a function that will be used to modify the state
     const [ counter, setCounter ] = useState(0);
 
-   const handleClick = () => {
-       console.log('click click!');
-   }
-
    const increaseByOne = () => setCounter(counter + 1);
+   const decreaseByOne = () => setCounter(counter - 1);
    const setToZero = () => setCounter(0);
 
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={increaseByOne}>
-                plus
-            </button>
-            <button onClick={setToZero}>
-                reset
-            </button>
+            <Display counter={counter} />
+            <Button onClick={increaseByOne} text={'plus'} />
+            <Button onClick={setToZero} text={'reset'} />
+            <Button onClick={decreaseByOne} text={'minus'} />
         </div>
     )
 }
